a56d25bfa3480f7ce90d982bf8b89276c47186ad
Fabian Rodriguez Cuauhizo <cuauhizo@gmail.com>
2024-01-31 20:11:28 -0600

    Integracion formulario, alerta

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
+import Formulario from './components/Formulario.vue';
 
 </script>
 
 <template>
-
+  <h1 class="titulo">Buscador de clima</h1>
+  <div class="contenedor buscador-clima">
+    <Formulario/>
+  </div>
 </template>
 
